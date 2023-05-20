5ab3cc7f209fc25158d6f07c2feb85ea34ffb40a
mancuoj <1337381768@qq.com>
2023-05-20 20:54:19 +0800

    update

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -8,11 +8,12 @@ export const sidebar = [
   {
     text: 'Angular',
     link: '/angular/',
-    collapsed: false,
+    // collapsed: false,
     items: [
       {
         text: 'Docs',
         link: '/angular/docs/',
+        // collapsed: false,
         items: [
           { text: 'Cheat Sheet', link: '/angular/docs/cheat-sheet' },
           { text: 'First App', link: '/angular/docs/first-app' },
@@ -24,42 +25,38 @@ export const sidebar = [
 
   {
     text: 'CSS',
-    collapsed: false,
+    link: '/css/',
+    // collapsed: false,
     items: [
-      { text: 'Overview', link: '/css/' },
     ]
   },
 
   {
     text: 'JavaScript',
-    collapsed: false,
+    link: '/js/',
+    // collapsed: false,
     items: [
-      { text: 'Overview', link: '/js/' },
-      {
-        text: 'Promise',
-        link: '/js/'
-      },
     ]
   },
 
   {
     text: 'React',
-    collapsed: true,
+    link: '/react/',
+    // collapsed: true,
     items: [
-      { text: 'Overview', link: '/react/' },
     ]
   },
 
   {
     text: 'Next',
+    link: '/next/',
     collapsed: true,
     items: [
       {
         text: 'Docs',
-        collapsed: true,
+        link: '/next/docs/',
         items: [
-          { text: 'Overview', link: '/next/' },
-          { text: 'Learn', link: '/next/learn' },
+          { text: 'Learn', link: '/next/docs/learn' },
         ]
       },
     ]
@@ -67,33 +64,33 @@ export const sidebar = [
 
   {
     text: 'Vue',
+    link: '/vue/',
     collapsed: true,
     items: [
-      { text: 'Overview', link: '/vue/' },
       { text: 'VitePress 速通', link: '/vue/vitepress' },
     ]
   },
 
   {
     text: 'Nuxt',
-    collapsed: true,
+    link: '/nuxt/',
+    // collapsed: true,
     items: [
-      { text: 'Overview', link: '/nuxt/' },
     ]
   },
 ]
 
 export const algoSidebar = [
-  { text: 'Overview', link: '/algo/' },
+  { text: '蠢人的总结', link: '/algo/' },
   {
     text: 'labuladong 的算法小抄',
     // collapsed: false,
     items: [
       {
         text: '二叉树',
+        link: '/algo/binary-tree/',
         // collapsed: false,
         items: [
-          { text: '蠢人的总结', link: '/algo/binary-tree/' },
           { text: '104. 二叉树的最大深度', link: '/algo/binary-tree/104' },
           { text: '144. 二叉树的前序遍历', link: '/algo/binary-tree/144' },
         ]
@@ -111,17 +108,18 @@ export const algoSidebar = [
     items: [
       {
         text: 'JavaScript',
+        link: '/algo/js/',
         // collapsed: false,
         items: [
-          { text: 'Overview', link: '/algo/js/' },
           { text: '2620. 计数器', link: '/algo/js/2620' },
           { text: '2621. 睡眠函数', link: '/algo/js/2621' },
         ]
       },
       {
         text: '剑指 Offer',
+        link: '/algo/offer/',
+        // collapsed: false,
         items: [
-          { text: 'Overview', link: '/algo/offer/' },
         ]
       },
     ]
@@ -143,16 +141,16 @@ export const interviewSidebar = [
 ]
 
 export const tschSidebar = [
-  { text: 'Overview', link: '/tsch/' },
+  { text: '蠢人的总结', link: '/tsch/' },
   { text: '13. Hello World', link: '/tsch/13' },
 ]
 
 export const hbsSidebar = [
   {
     text: '第 1 章：什么是 JavaScript',
+    link: '/hbs/1/',
     collapsed: true,
     items: [
-      { text: '1.0 本章内容', link: '/hbs/1/' },
       { text: '1.1 简短的历史回顾', link: '/hbs/1/1' },
       { text: '1.2 JavaScript 实现', link: '/hbs/1/2' },
       { text: '1.3 JavaScript 版本', link: '/hbs/1/3' },
@@ -161,9 +159,9 @@ export const hbsSidebar = [
   },
   {
     text: '第 2 章：HTML 中的 JavaScript',
+    link: '/hbs/2/',
     collapsed: true,
     items: [
-      { text: '2.0 本章内容', link: '/hbs/2/' },
       { text: '2.1 script 元素', link: '/hbs/2/1' },
       { text: '2.2 行内代码与外部文件', link: '/hbs/2/2' },
       { text: '2.3 文档模式', link: '/hbs/2/3' },
@@ -173,9 +171,9 @@ export const hbsSidebar = [
   },
   {
     text: '第 3 章：语言基础',
+    link: '/hbs/3/',
     collapsed: true,
     items: [
-      { text: '3.0 本章内容', link: '/hbs/3/' },
       { text: '3.1 语法', link: '/hbs/3/1' },
       { text: '3.2 关键字与保留字', link: '/hbs/3/2' },
       { text: '3.3 变量', link: '/hbs/3/3' },
@@ -188,9 +186,9 @@ export const hbsSidebar = [
   },
   {
     text: '第 4 章：变量、作用域与内存',
+    link: '/hbs/4/',
     collapsed: true,
     items: [
-      { text: '4.0 本章内容', link: '/hbs/4/' },
       { text: '4.1 原始值与引用值', link: '/hbs/4/1' },
       { text: '4.2 执行上下文与作用域', link: '/hbs/4/2' },
       { text: '4.3 垃圾回收', link: '/hbs/4/3' },
@@ -199,9 +197,9 @@ export const hbsSidebar = [
   },
   {
     text: '第 5 章：基本引用类型',
+    link: '/hbs/5/',
     collapsed: true,
     items: [
-      { text: '5.0 本章内容', link: '/hbs/5/' },
       { text: '5.1 Date', link: '/hbs/5/1' },
       { text: '5.2 RegExp', link: '/hbs/5/2' },
       { text: '5.3 原始值包装类型', link: '/hbs/5/3' },
@@ -211,9 +209,9 @@ export const hbsSidebar = [
   },
   {
     text: '第 6 章：集合引用类型',
+    link: '/hbs/6/',
     collapsed: true,
     items: [
-      { text: '6.0 本章内容', link: '/hbs/6/' },
       { text: '6.1 Object', link: '/hbs/6/1' },
       { text: '6.2 Array', link: '/hbs/6/2' },
       { text: '6.3 定型数组', link: '/hbs/6/3' },
@@ -227,9 +225,9 @@ export const hbsSidebar = [
   },
   {
     text: '第 7 章：迭代器与生成器',
+    link: '/hbs/7/',
     collapsed: true,
     items: [
-      { text: '7.0 本章内容', link: '/hbs/7/' },
       { text: '7.1 理解迭代', link: '/hbs/7/1' },
       { text: '7.2 迭代器模式', link: '/hbs/7/2' },
       { text: '7.3 生成器', link: '/hbs/7/3' },
@@ -238,9 +236,9 @@ export const hbsSidebar = [
   },
   {
     text: '第 8 章：对象、类与面向对象编程',
+    link: '/hbs/8/',
     collapsed: true,
     items: [
-      { text: '8.0 本章内容', link: '/hbs/8/' },
       { text: '8.1 理解对象', link: '/hbs/8/1' },
       { text: '8.2 创建对象', link: '/hbs/8/2' },
       { text: '8.3 继承', link: '/hbs/8/3' },
@@ -253,9 +251,9 @@ export const hbsSidebar = [
 export const xnsSidebar = [
   {
     text: '第 1 章：JavaScript 简介',
-    collapsed: true,
+    link: '/xns/1/',
+    collapsed: false,
     items: [
-      { text: '1.0 本章内容', link: '/xns/1/' },
       { text: '1.1 探索 JavaScript', link: '/xns/1/1' },
       { text: '1.2 Hello World', link: '/xns/1/2' },
       { text: '1.3 JavaScript 之旅', link: '/xns/1/3' },
@@ -265,124 +263,124 @@ export const xnsSidebar = [
   },
   {
     text: '第 2 章：词法结构',
+    link: '/xns/2/',
     collapsed: true,
     items: [
-      { text: '2.0 本章内容', link: '/xns/2/' },
     ]
   },
   {
     text: '第 3 章：类型、值和变量',
+    link: '/xns/3/',
     collapsed: true,
     items: [
-      { text: '3.0 本章内容', link: '/xns/3/' },
     ]
   },
   {
     text: '第 4 章：表达式和运算符',
+    link: '/xns/4/',
     collapsed: true,
     items: [
-      { text: '4.0 本章内容', link: '/xns/4/' },
     ]
   },
   {
     text: '第 5 章：语句',
+    link: '/xns/5/',
     collapsed: true,
     items: [
-      { text: '5.0 本章内容', link: '/xns/5/' },
     ]
   },
   {
     text: '第 6 章：对象',
+    link: '/xns/6/',
     collapsed: true,
     items: [
-      { text: '6.0 本章内容', link: '/xns/6/' },
     ]
   },
-  {
-    text: '第 7 章：数组',
-    collapsed: true,
-    items: [
-      { text: '7.0 本章内容', link: '/xns/7/' },
-    ]
-  },
-  {
-    text: '第 8 章：函数',
-    collapsed: true,
-    items: [
-      { text: '8.0 本章内容', link: '/xns/8/' },
-    ]
-  },
-  {
-    text: '第 9 章：类',
-    collapsed: true,
-    items: [
-      { text: '9.0 本章内容', link: '/xns/9/' },
-    ]
-  },
-  {
-    text: '第 10 章：模块',
-    collapsed: true,
-    items: [
-      { text: '10.0 本章内容', link: '/xns/10/' },
-    ]
-  },
-  {
-    text: '第 11 章：JavaScript 标准库',
-    collapsed: true,
-    items: [
-      { text: '11.0 本章内容', link: '/xns/11/' },
-    ]
-  },
-  {
-    text: '第 12 章：迭代器和生成器',
-    collapsed: true,
-    items: [
-      { text: '12.0 本章内容', link: '/xns/12/' },
-    ]
-  },
-  {
-    text: '第 13 章：异步 JavaScript',
-    collapsed: true,
-    items: [
-      { text: '13.0 本章内容', link: '/xns/13/' },
-    ]
-  },
-  {
-    text: '第 14 章：元编程',
-    collapsed: true,
-    items: [
-      { text: '14.0 本章内容', link: '/xns/14/' },
-    ]
-  },
-  {
-    text: '第 15 章：浏览器中的 JavaScript',
-    collapsed: true,
-    items: [
-      { text: '15.0 本章内容', link: '/xns/15/' },
-    ]
-  },
-  {
-    text: '第 16 章：Node 服务器端 JavaScript',
-    collapsed: true,
-    items: [
-      { text: '16.0 本章内容', link: '/xns/16/' },
-    ]
-  },
-  {
-    text: '第 17 章：JavaScript 工具和扩展',
-    collapsed: true,
-    items: [
-      { text: '17.0 本章内容', link: '/xns/17/' },
-    ]
-  },
+  // {
+  //   text: '第 7 章：数组',
+  //   link: '/xns/7/',
+  //   collapsed: true,
+  //   items: [,
+  //   ]
+  // },
+  // {
+  //   text: '第 8 章：函数',
+  //   link: '/xns/8/',
+  //   collapsed: true,
+  //   items: [
+  //   ]
+  // },
+  // {
+  //   text: '第 9 章：类',
+  //   link: '/xns/9/',
+  //   collapsed: true,
+  //   items: [
+  //   ]
+  // },
+  // {
+  //   text: '第 10 章：模块',
+  //   link: '/xns/10/',
+  //   collapsed: true,
+  //   items: [
+  //   ]
+  // },
+  // {
+  //   text: '第 11 章：JavaScript 标准库',
+  //   link: '/xns/11/',
+  //   collapsed: true,
+  //   items: [
+  //   ]
+  // },
+  // {
+  //   text: '第 12 章：迭代器和生成器',
+  //   link: '/xns/12/',
+  //   collapsed: true,
+  //   items: [
+  //   ]
+  // },
+  // {
+  //   text: '第 13 章：异步 JavaScript',
+  //   link: '/xns/13/',
+  //   collapsed: true,
+  //   items: [
+  //   ]
+  // },
+  // {
+  //   text: '第 14 章：元编程',
+  //   link: '/xns/14/',
+  //   collapsed: true,
+  //   items: [
+  //   ]
+  // },
+  // {
+  //   text: '第 15 章：浏览器中的 JavaScript',
+  //   link: '/xns/15/',
+  //   collapsed: true,
+  //   items: [
+  //   ]
+  // },
+  // {
+  //   text: '第 16 章：Node 服务器端 JavaScript',
+  //   link: '/xns/16/',
+  //   collapsed: true,
+  //   items: [
+  //   ]
+  // },
+  // {
+  //   text: '第 17 章：JavaScript 工具和扩展',
+  //   link: '/xns/17/',
+  //   collapsed: true,
+  //   items: [
+  //   ]
+  // },
 ]
 
 export const jsdpSidebar = [
   {
     text: '第 1 章：面向对象的JavaScript',
+    link: '/jsdp/1/',
     collapsed: true,
     items: [
-      { text: '1.0 本章内容', link: '/jsdp/1/' },
       { text: '1.1 动态类型语言和鸭子类型', link: '/jsdp/1/1' },
       { text: '1.2 多态', link: '/jsdp/1/2' },
       { text: '1.3 封装', link: '/jsdp/1/3' },
--- src/react/index.md
+++ src/react/index.md
@@ -1,1 +1,1 @@
-# Overview
+# React
