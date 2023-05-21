13f8c556ab16b3952bfb58bfb1a52f6c479891c1
mancuoj <1337381768@qq.com>
2023-05-22 00:03:34 +0800

    update

--- .vitepress/navbar.ts
+++ .vitepress/navbar.ts
@@ -12,19 +12,19 @@ export const navbar = [
         ],
       },
       {
-        text: '掘金小册',
-        items: [
-          { text: 'TS 类型体操通关秘籍', link: '/juejin/tsch/', activeMatch: '/juejin/tsch' },
-          { text: 'TS 全面进阶指南', link: '/juejin/ts/', activeMatch: '/juejin/ts/' },
-        ],
-      },
-      {
         text: '在线阅读',
         items: [
           { text: 'ES6 入门', link: 'https://es6.ruanyifeng.com/#docs/intro' },
           { text: 'javascript.info', link: 'https://zh.javascript.info/' },
           { text: 'patterns.dev', link: 'https://www.patterns.dev/' },
           { text: 'JavaScript 二十年', link: 'https://cn.history.js.org/' },
+        ],
+      },
+      {
+        text: '掘金小册',
+        items: [
+          { text: 'TS 类型体操通关秘籍', link: '/juejin/tsch/', activeMatch: '/juejin/tsch' },
+          { text: 'TS 全面进阶指南', link: '/juejin/ts/', activeMatch: '/juejin/ts/' },
         ],
       },
     ],
