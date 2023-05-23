eee6f88e44ac0e8eaf525fba5a983f08b9fd6ef2
mancuoj <1337381768@qq.com>
2023-05-24 01:30:04 +0800

    update

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -165,103 +165,262 @@ export const tschSidebar = [
 
 export const hbsSidebar = [
   {
-    text: '第 1 章：什么是 JavaScript',
-    link: '/hbs/1/',
-    collapsed: true,
+    text: '语言特性',
+    // collapsed: true,
     items: [
-      { text: '1.1 简短的历史回顾', link: '/hbs/1/1' },
-      { text: '1.2 JavaScript 实现', link: '/hbs/1/2' },
-      { text: '1.3 JavaScript 版本', link: '/hbs/1/3' },
-      { text: '1.4 小结', link: '/hbs/1/4' },
+      {
+        text: '第 4 章：变量、作用域与内存',
+        link: '/hbs/4/',
+        collapsed: true,
+        items: [
+          { text: '4.1 原始值与引用值', link: '/hbs/4/1' },
+          { text: '4.2 执行上下文与作用域', link: '/hbs/4/2' },
+          { text: '4.3 垃圾回收', link: '/hbs/4/3' },
+          { text: '4.4 小结', link: '/hbs/4/4' },
+        ],
+      },
+      {
+        text: '第 5 章：基本引用类型',
+        link: '/hbs/5/',
+        collapsed: true,
+        items: [
+          { text: '5.1 Date', link: '/hbs/5/1' },
+          { text: '5.2 RegExp', link: '/hbs/5/2' },
+          { text: '5.3 原始值包装类型', link: '/hbs/5/3' },
+          { text: '5.4 单例内置对象', link: '/hbs/5/4' },
+          { text: '5.5 小结', link: '/hbs/5/5' },
+        ],
+      },
+      {
+        text: '第 6 章：集合引用类型',
+        link: '/hbs/6/',
+        collapsed: true,
+        items: [
+          { text: '6.1 Object', link: '/hbs/6/1' },
+          { text: '6.2 Array', link: '/hbs/6/2' },
+          { text: '6.3 定型数组', link: '/hbs/6/3' },
+          { text: '6.4 Map', link: '/hbs/6/4' },
+          { text: '6.5 WeakMap', link: '/hbs/6/5' },
+          { text: '6.6 Set', link: '/hbs/6/6' },
+          { text: '6.7 WeakSet', link: '/hbs/6/7' },
+          { text: '6.8 迭代与扩展操作', link: '/hbs/6/8' },
+          { text: '6.9 小结', link: '/hbs/6/9' },
+        ],
+      },
+      {
+        text: '第 7 章：迭代器与生成器',
+        link: '/hbs/7/',
+        collapsed: true,
+        items: [
+          { text: '7.1 理解迭代', link: '/hbs/7/1' },
+          { text: '7.2 迭代器模式', link: '/hbs/7/2' },
+          { text: '7.3 生成器', link: '/hbs/7/3' },
+          { text: '7.4 小结', link: '/hbs/7/4' },
+        ],
+      },
+      {
+        text: '第 8 章：对象、类与面向对象编程',
+        link: '/hbs/8/',
+        collapsed: true,
+        items: [
+          { text: '8.1 理解对象', link: '/hbs/8/1' },
+          { text: '8.2 创建对象', link: '/hbs/8/2' },
+          { text: '8.3 继承', link: '/hbs/8/3' },
+          { text: '8.4 类', link: '/hbs/8/4' },
+          { text: '8.5 小结', link: '/hbs/8/5' },
+        ],
+      },
+      {
+        text: '第 9 章：代理与反射',
+        link: '/hbs/9/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 10 章：代理与反射',
+        link: '/hbs/10/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 11 章：期约与异步函数',
+        link: '/hbs/11/',
+        collapsed: true,
+        items: [
+        ],
+      },
     ],
   },
+
   {
-    text: '第 2 章：HTML 中的 JavaScript',
-    link: '/hbs/2/',
-    collapsed: true,
+    text: '提高',
+    // collapsed: true,
     items: [
-      { text: '2.1 script 元素', link: '/hbs/2/1' },
-      { text: '2.2 行内代码与外部文件', link: '/hbs/2/2' },
-      { text: '2.3 文档模式', link: '/hbs/2/3' },
-      { text: '2.4 noscript 元素', link: '/hbs/2/4' },
-      { text: '2.5 小结', link: '/hbs/2/5' },
+      {
+        text: '第 12 章：BOM',
+        link: '/hbs/12/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 14 章：DOM',
+        link: '/hbs/14/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 15 章：DOM 扩展',
+        link: '/hbs/15/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 16 章：DOM2 和 DOM3',
+        link: '/hbs/16/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 17 章：事件',
+        link: '/hbs/17/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 19 章：表单脚本',
+        link: '/hbs/19/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 23 章：JSON',
+        link: '/hbs/23/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 24 章：网络请求与远程资源',
+        link: '/hbs/24/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 25 章：客户端存储',
+        link: '/hbs/25/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 26 章：模块',
+        link: '/hbs/26/',
+        collapsed: true,
+        items: [
+        ],
+      },
     ],
   },
+
   {
-    text: '第 3 章：语言基础',
-    link: '/hbs/3/',
-    collapsed: true,
+    text: '其他',
     items: [
-      { text: '3.1 语法', link: '/hbs/3/1' },
-      { text: '3.2 关键字与保留字', link: '/hbs/3/2' },
-      { text: '3.3 变量', link: '/hbs/3/3' },
-      { text: '3.4 数据类型', link: '/hbs/3/4' },
-      { text: '3.5 操作符', link: '/hbs/3/5' },
-      { text: '3.6 语句', link: '/hbs/3/6' },
-      { text: '3.7 函数', link: '/hbs/3/7' },
-      { text: '3.8 小结', link: '/hbs/3/8' },
-    ],
-  },
-  {
-    text: '第 4 章：变量、作用域与内存',
-    link: '/hbs/4/',
-    collapsed: true,
-    items: [
-      { text: '4.1 原始值与引用值', link: '/hbs/4/1' },
-      { text: '4.2 执行上下文与作用域', link: '/hbs/4/2' },
-      { text: '4.3 垃圾回收', link: '/hbs/4/3' },
-      { text: '4.4 小结', link: '/hbs/4/4' },
-    ],
-  },
-  {
-    text: '第 5 章：基本引用类型',
-    link: '/hbs/5/',
-    collapsed: true,
-    items: [
-      { text: '5.1 Date', link: '/hbs/5/1' },
-      { text: '5.2 RegExp', link: '/hbs/5/2' },
-      { text: '5.3 原始值包装类型', link: '/hbs/5/3' },
-      { text: '5.4 单例内置对象', link: '/hbs/5/4' },
-      { text: '5.5 小结', link: '/hbs/5/5' },
-    ],
-  },
-  {
-    text: '第 6 章：集合引用类型',
-    link: '/hbs/6/',
-    collapsed: true,
-    items: [
-      { text: '6.1 Object', link: '/hbs/6/1' },
-      { text: '6.2 Array', link: '/hbs/6/2' },
-      { text: '6.3 定型数组', link: '/hbs/6/3' },
-      { text: '6.4 Map', link: '/hbs/6/4' },
-      { text: '6.5 WeakMap', link: '/hbs/6/5' },
-      { text: '6.6 Set', link: '/hbs/6/6' },
-      { text: '6.7 WeakSet', link: '/hbs/6/7' },
-      { text: '6.8 迭代与扩展操作', link: '/hbs/6/8' },
-      { text: '6.9 小结', link: '/hbs/6/9' },
-    ],
-  },
-  {
-    text: '第 7 章：迭代器与生成器',
-    link: '/hbs/7/',
-    collapsed: true,
-    items: [
-      { text: '7.1 理解迭代', link: '/hbs/7/1' },
-      { text: '7.2 迭代器模式', link: '/hbs/7/2' },
-      { text: '7.3 生成器', link: '/hbs/7/3' },
-      { text: '7.4 小结', link: '/hbs/7/4' },
-    ],
-  },
-  {
-    text: '第 8 章：对象、类与面向对象编程',
-    link: '/hbs/8/',
-    collapsed: true,
-    items: [
-      { text: '8.1 理解对象', link: '/hbs/8/1' },
-      { text: '8.2 创建对象', link: '/hbs/8/2' },
-      { text: '8.3 继承', link: '/hbs/8/3' },
-      { text: '8.4 类', link: '/hbs/8/4' },
-      { text: '8.5 小结', link: '/hbs/8/5' },
+      {
+        text: '第 1 章：什么是 JavaScript',
+        link: '/hbs/1/',
+        collapsed: true,
+        items: [
+          { text: '1.1 简短的历史回顾', link: '/hbs/1/1' },
+          { text: '1.2 JavaScript 实现', link: '/hbs/1/2' },
+          { text: '1.3 JavaScript 版本', link: '/hbs/1/3' },
+          { text: '1.4 小结', link: '/hbs/1/4' },
+        ],
+      },
+      {
+        text: '第 2 章：HTML 中的 JavaScript',
+        link: '/hbs/2/',
+        collapsed: true,
+        items: [
+          { text: '2.1 script 元素', link: '/hbs/2/1' },
+          { text: '2.2 行内代码与外部文件', link: '/hbs/2/2' },
+          { text: '2.3 文档模式', link: '/hbs/2/3' },
+          { text: '2.4 noscript 元素', link: '/hbs/2/4' },
+          { text: '2.5 小结', link: '/hbs/2/5' },
+        ],
+      },
+      {
+        text: '第 3 章：语言基础',
+        link: '/hbs/3/',
+        collapsed: true,
+        items: [
+          { text: '3.1 语法', link: '/hbs/3/1' },
+          { text: '3.2 关键字与保留字', link: '/hbs/3/2' },
+          { text: '3.3 变量', link: '/hbs/3/3' },
+          { text: '3.4 数据类型', link: '/hbs/3/4' },
+          { text: '3.5 操作符', link: '/hbs/3/5' },
+          { text: '3.6 语句', link: '/hbs/3/6' },
+          { text: '3.7 函数', link: '/hbs/3/7' },
+          { text: '3.8 小结', link: '/hbs/3/8' },
+        ],
+      },
+      {
+        text: '第 13 章：客户端检测',
+        link: '/hbs/13/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 18 章：动画与 Canvas 图形',
+        link: '/hbs/18/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 20 章：JavaScript API',
+        link: '/hbs/20/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 21 章：错误处理与调试',
+        link: '/hbs/21/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 22 章：处理 XML',
+        link: '/hbs/22/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 27 章：工作者线程',
+        link: '/hbs/27/',
+        collapsed: true,
+        items: [
+        ],
+      },
+      {
+        text: '第 28 章：最佳实践',
+        link: '/hbs/28/',
+        collapsed: true,
+        items: [
+        ],
+      },
     ],
   },
 ]
