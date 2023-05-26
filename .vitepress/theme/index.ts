a6339c6646cafa2e076fafc3486070b17e0aa486
mancuoj <1337381768@qq.com>
2023-05-26 09:25:48 +0800

    update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -8,6 +8,7 @@ import './overrides.css'
 import '@fontsource/jetbrains-mono'
 import '@fontsource/merriweather/400.css'
 import '@fontsource/merriweather/700.css'
+import 'lxgw-wenkai-screen-webfont/style.css'
 
 export default {
   ...Theme,
